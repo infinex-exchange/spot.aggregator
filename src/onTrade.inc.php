f266a156cb7f12df554d92e0f21fe9620d8f730a
vayamos-exchange <admin@vayamos.cc>
2022-11-28 16:46:57 +0100

    Debug

--- src/onTrade.inc.php
+++ src/onTrade.inc.php
@@ -2,12 +2,17 @@
 
 use PhpAmqpLib\Message\AMQPMessage;
 
-function onTrade(AMQPMessage $msgIn) { 
+function onTrade(AMQPMessage $msgIn) {
+    global $debug;
+     
     try {
         $body = json_decode($msgIn -> body, true);
         $headers = $msgIn->get('application_headers')->getNativeData();
     
-        echo "Headers:\n".json_encode($headers, JSON_PRETTY_PRINT)."\nBody:\n".$msgIn->body."\n\n";
+        if($debug) echo "Headers:\n".json_encode($headers, JSON_PRETTY_PRINT)."\nBody:\n".$msgIn->body."\n\n";
+        
+        echo $headers -> pairid;
+        echo $msgIn -> body -> price;
         
         $msgIn -> ack();
     }
